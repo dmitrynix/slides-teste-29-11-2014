1902491095b3032312e1159e9c2e90b6ae262f48
Dmitry Nix <dmitry.nix@gmail.com>
2014-11-30 12:23:44 -0300

    Lasts fixes before talk.

--- clientes/spec/features/customer_spec.rb
+++ clientes/spec/features/customer_spec.rb
@@ -4,7 +4,8 @@ feature 'Customer' do
   scenario 'should be able to create customer', :js => true do
     visit '/customers'                   ; page.driver.render '/tmp/1-visit.png', :full => true
     click_link 'New Customer'            ; page.driver.render '/tmp/2-click.png', :full => true
-    fill_in 'Name', :with => 'Alice'     ; page.driver.render '/tmp/3-fill.png', :full => true
+    fill_in 'Name', :with => 'Carla'     ; page.driver.render '/tmp/3-fill.png', :full => true
     click_button 'Create'                ; page.driver.render '/tmp/4-create.png', :full => true
+    expect(page).to have_content 'Carla'
   end
 end
